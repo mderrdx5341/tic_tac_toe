3f643bb70b39438824307c81f1b85fd1f00bb561
Roman <mderrdx5341@gmail.com>
2023-10-20 11:24:33 +0300

    delete comments

--- public/index.php
+++ public/index.php
@@ -2,17 +2,3 @@
 include "../autoload.php";
 $app = new \Core\App();
 $app->run();
-/*
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="app.css">
-        <script type="text/javascript" charset="UTF-8" src="app.js"></script>
-    </head>
-    <body>
-        <div id="app">
-        </div>
-    </body>
-</html>
-*/
