c799e0bdf5583b5631b63619c3c429da5177cd5f
Roman <mderrdx5341@gmail.com>
2023-10-25 14:30:46 +0300

    changed start websocket server

--- websoket.start.php
+++ websoket.start.php
@@ -2,9 +2,6 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/backend/bootstrap.php');
 
-$playersDBMock = new Project\PlayersDBMock();
-$playersRepository = new Project\WebSocket\PlayersRepository();
-
 $db = new Project\WebSocket\PlayersDB();
 $db->run();
 $wss = new Project\WebSocket\Server($db);
