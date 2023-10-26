88de162e008432541687ffaf56f4ddf077c8a9eb
Roman <mderrdx5341@gmail.com>
2023-10-26 09:17:18 +0300

    refactoring

---
+++ backend/Project/WebSocket/PlayersDBClient.php
@@ -0,0 +1,40 @@
+<?php
+namespace Project\WebSocket;
+
+use OpenSwoole\Core\Coroutine\Client\PDOConfig;
+use OpenSwoole\Core\Coroutine\Client\PDOClient;
+
+// mysql -usite -psite -h192.168.234.95
+class PlayersDBClient
+{
+    private $config;
+    private $client;
+
+    public function connect(): void
+    {
+        $config = (new PDOConfig())
+            ->withHost('192.168.224.1')
+            // ->withUnixSocket('/tmp/mysql.sock')
+            ->withDbName('tic_tac_toe')
+            ->withCharset('utf8mb4')
+            ->withUsername('site')
+            ->withPassword('site');
+
+        $this->client = new PDOClient($config);
+    }
+
+    public function playerResponseDB()
+    {
+        return $this->client->query('SELECT * from players');
+    }
+
+    public function players()
+    {
+        $playersResponse = $this->client->query('SELECT * from players');
+        $players = [];
+        while($player = $players->fetch()) {
+            $players[] = $player;
+        }
+        return $players;
+    }
+}
