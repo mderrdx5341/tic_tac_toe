284e6eccc5bc1041e4cb500a458ce5c9274211f2
Roman <mderrdx5341@gmail.com>
2023-10-26 09:12:08 +0300

    rename class name

--- websoket.start.php
+++ websoket.start.php
@@ -2,8 +2,8 @@
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/backend/bootstrap.php');
 
-$db = new Project\WebSocket\PlayersDB();
-$db->run();
+$db = new Project\WebSocket\PlayersDBClient();
+$db->connect();
 $wss = new Project\WebSocket\Server($db);
 
 $wss->start();
